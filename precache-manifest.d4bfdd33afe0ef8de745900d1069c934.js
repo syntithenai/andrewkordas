dea2c10e9e3bb6404d6662314ffc6f9f01b3fbcd
Steve Ryan <syntithenai@gmail.com>
2022-05-31 23:14:14 +1000

    build

---
+++ precache-manifest.d4bfdd33afe0ef8de745900d1069c934.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dc1c98c47f2d46b3db2",
+    "url": "/static/css/main.463be71b.chunk.css"
+  },
+  {
+    "revision": "0dc1c98c47f2d46b3db2",
+    "url": "/static/js/main.0dc1c98c.chunk.js"
+  },
+  {
+    "revision": "b4580c6e423a71b044a8",
+    "url": "/static/js/1.b4580c6e.chunk.js"
+  },
+  {
+    "revision": "fe43042a527cfe3ce987",
+    "url": "/static/css/2.05be9ee1.chunk.css"
+  },
+  {
+    "revision": "fe43042a527cfe3ce987",
+    "url": "/static/js/2.fe43042a.chunk.js"
+  },
+  {
+    "revision": "9ebd49d8dd78d63c29fc",
+    "url": "/static/js/runtime~main.9ebd49d8.js"
+  },
+  {
+    "revision": "c6df1dd4d9920affb32b2401d13c0e88",
+    "url": "/index.html"
+  }
+];
